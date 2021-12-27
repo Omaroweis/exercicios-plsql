427bca9c0b908872b945072656b8ed532723aad3
Omar <omararaujolopes1@gmail.com>
2021-12-27 14:48:50 -0300

    Create 1.sql

---
+++ 1.sql
@@ -0,0 +1,11 @@
+ -- ATIVIDADE 01
+ -- criar uma function para validas se um parametro enviado é par ou impar
+-- retorna 0 para false e 1 para true
+create or replace function ehPar(P_numero in number) return number
+is
+begin
+    if mod(p_numero, 2) = 0 then
+    return 1;
+    end if;
+    return 0;
+end;
