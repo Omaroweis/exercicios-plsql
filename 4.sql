0a890f0d3b8dd1e484ff6f9e40b5d0b8d466e4fa
Omar <omararaujolopes1@gmail.com>
2021-12-28 14:12:13 -0300

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -1,63 +1,54 @@
-create or replace function validacao(p_cnpj varchar2, p_verificador varchar2, p_referencia number) return number
+--exercicio 4
+-- valida cnpj
+set serveroutput on;
+create or replace function validador(p_cnpj varchar, p_verificador varchar, p_aux number) return number
 is
-cnpj_numeral number;
-soma number;
-contador number;
-i number;
-begin
-    i := p_referencia;
+    aux number;
+    cnpj_numeral number;
+    soma number;
+begin 
+    aux := p_aux;
     soma := 0;
-    contador := 1;
-    if p_referencia = 5 then
-        for j in 1..4 loop
-            cnpj_numeral := to_number(substr(p_cnpj, contador,1));
-            contador := contador +1;
-            soma := soma + (cnpj_numeral * i);
-            i := i-1;
-        end loop;
-    else
-         for j in 1..5 loop
-            cnpj_numeral := to_number(substr(p_cnpj, contador,1));
-            contador := contador +1;
-            soma := soma + (cnpj_numeral * i);
-            i := i-1;
-         end loop;
-    i := 9;
-    for j in 2 .. 9 loop
-        cnpj_numeral := to_number(substr(p_cnpj, contador,1));
-        contador := contador +1;
-        soma := soma + (cnpj_numeral * i);
-        i := i-1;
+    for i in 1 .. (aux-1) loop
+        cnpj_numeral := to_number(substr(p_cnpj, i, 1));
+        soma := soma + (cnpj_numeral * aux);
+        --DBMS_OUTPUT.PUT_LINE(soma);
+        aux:= aux -1;
     end loop;
+    aux := 9;
+    for i in p_aux .. p_aux+7 loop
+        cnpj_numeral := to_number(substr(p_cnpj, i, 1));
+        soma := soma + (cnpj_numeral * aux);
+        aux := aux -1;
+        --DBMS_OUTPUT.PUT_LINE(soma);
+    end loop;
+    
     soma := mod(soma, 11);
-    if soma < 2 then
-        soma := 0;
-    else
-        soma := 11 - soma;
+    if soma <2 then soma:=0;
+    else soma:= 11-soma;
     end if;
-    if soma = p_verificador then 
-        return soma;
+    
+    if soma = to_number(p_verificador) then return 1;
+    else return 0;
     end if;
-    return soma;   
     
 end;
 
-create or replace function validacnpj(p_cnpj in varchar2) return number
+create or replace function validarCNPJ(p_cnpj varchar) return number
 is
-primeiro number;
-segundo number;
-begin
-    primeiro := validacao(p_cnpj, substr(p_cnpj, 13,1), 5);
-    segundo := validacao(p_cnpj, substr(p_cnpj, 14,1), 6);
-    
-    if primeiro =1 and segundo =1 then
+    primeiro number;
+    segundo number;
+begin 
+    primeiro := validador(p_cnpj, substr(p_cnpj, 13, 1), 5);
+    segundo := validador(p_cnpj, substr(p_cnpj, 14, 1), 6);
+    if primeiro = 1 and segundo = 1 then
         return 1;
     end if;
     return 0;
 end;
 
-variable resposta number;
+variable teste number;
 
-execute :resposta := validacao('53113791000122', '2', 5);
+execute :teste := validarCNPJ('INSIRA CNPJ');
 
-print resposta;
+print teste;
